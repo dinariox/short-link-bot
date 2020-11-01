d5ee15dbd7abd6a41eefb18a82387baa716723a7
Timo Nowak <timo.n.2000.de@gmail.com>
2020-11-01 02:46:09 +0100

    Working Amazon link shortening

---
+++ README.md
@@ -0,0 +1,7 @@
+# Link Shortening Bot for Discord
+
+This is a bot for Discord servers. It scans messages sent by users in all channels it has access to. If it detects a "amazon.de" or "amazon.com" url, it will delete the message and send a shortened version of that link. The bit.ly API is used for shortening the urls.
+
+## Future
+
+In the future, general link shortening may be implemented.
---
+++ index.js
@@ -0,0 +1,57 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const axios = require("axios");
+
+const config = JSON.parse(fs.readFileSync("config.json", "utf-8"));
+const client = new Discord.Client();
+
+const DEFAULT_USERNAME = "Link Shortening Bot";
+
+client.on("ready", () => {
+	console.log(client.user.username + " ready");
+});
+
+client.on("message", (msg) => {
+	if (msg.author.id === client.user.id) return;
+
+	const content = msg.content;
+	if (content.match(/amazon\.de\/|amazon\.com\//gi)) {
+		getShortenedLink(msg.content, (err, shortLink) => {
+			if (err) return;
+
+			const embed = new Discord.MessageEmbed();
+			embed.setTitle(msg.author.username);
+			embed.addField("Amazon", shortLink);
+			embed.setColor(msg.member.displayHexColor);
+
+			msg.channel.send(embed);
+
+			msg.delete();
+		});
+	}
+});
+
+client.login(config.token);
+
+function getShortenedLink(longLink, callback) {
+	let postReq = {
+		method: "post",
+		url: "https://api-ssl.bitly.com/v4/shorten",
+		headers: {
+			Authorization: "Bearer e0d9271cf380960190cce917626580fe7c83d791",
+			"Content-Type": "application/json",
+		},
+		data: {
+			domain: "bit.ly",
+			long_url: longLink,
+		},
+	};
+
+	axios(postReq)
+		.then((resp) => {
+			callback(null, resp.data.link);
+		})
+		.catch((err) => {
+			console.log(err, null);
+		});
+}
